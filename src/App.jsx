62f1abd4b5c47be71b5681be6d93207f06a6bad3
Eder-Sousa <ederdi395@gmail.com>
2024-03-04 11:49:49 +0000

    Copilot 4 commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Eder Sousa",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "ederdi395@gmail.com",
+  gitHub: "Eder-Sousa",
+  instagram: "Eder-Sousa",
+  linkedIn: "Eder-Sousa",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Eder-Sousa",
+  youTube: "Eder-Sousa",
 };
 
 const primaryColor = "#4E567E";
